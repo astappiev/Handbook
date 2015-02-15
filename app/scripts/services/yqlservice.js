36d7aebd4db55a5532f021d4e534abf05740a309
Oleg Astappev <astappev@gmail.com>
2015-02-15 21:22:57 +0200

    Update date range

--- app/scripts/services/yqlservice.js
+++ app/scripts/services/yqlservice.js
@@ -11,10 +11,26 @@ angular.module('handbook')
     .factory('yqlService', function ($http) {
         return {
             getTwitterGC: function() {
-                return $http.get('https://query.yahooapis.com/v1/public/yql/astappev/handbook-gc_media?format=json&callback=');
+                return $http.get('https://query.yahooapis.com/v1/public/yql/astappev/handbook-gc_media?format=json');
             },
             getAnalytics: function() {
-                return $http.get('https://query.yahooapis.com/v1/public/yql/astappev/gcbook-top50?format=json&callback=');
+
+                function getDay(date) {
+                    var day = date.getDate();
+                    return day < 10 ? '0' + day : day;
+                }
+                function getMonth(date) {
+                    var month = date.getMonth() + 1;
+                    return month < 10 ? '0' + month : month;
+                }
+
+                var start = new Date();
+                var end = new Date();
+                start.setMonth(start.getMonth() - 1);
+                var endStr = end.getFullYear() + '-' + getMonth(end) + '-' + getDay(end);
+                var startStr = start.getFullYear() + '-' + getMonth(start) + '-' + getDay(start);
+
+                return $http.get('https://query.yahooapis.com/v1/public/yql/astappev/gcbook-analytics?start=' + startStr + '&end=' + endStr + '&format=json');
             }
         };
     });
